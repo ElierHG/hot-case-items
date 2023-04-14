453c05e025b684a9e743cc940655736f167aa91b
Elier Herrera <elierh@milamsmarkets.com>
2023-04-14 11:27:00 -0400

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'hot-case-item-list',
+  base: 'hot-case-items',
   plugins: [svelte()],
 })
